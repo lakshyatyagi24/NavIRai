fbe84dcc096502d6a99b3e182e5fad7803c8f1ea
Lakshya Tyagi <tyagicoolboy22@gmail.com>
2020-05-26 15:16:12 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/navirai/Login_form.kt
@@ -0,0 +1,12 @@
+package com.example.navirai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login_form : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_form)
+    }
+}
---
+++ app/src/main/java/com/example/navirai/Signup_form.kt
@@ -0,0 +1,12 @@
+package com.example.navirai
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Signup_form : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_form)
+    }
+}
